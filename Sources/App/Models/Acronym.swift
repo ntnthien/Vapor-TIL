bc72e62653fed74783ddaf46e7bce61cfdd181b2
Do Nguyen <daniel@zincer.com>
2018-05-27 23:20:39 +0700

    Use PostgreSQL as db

--- Sources/App/Models/Acronym.swift
+++ Sources/App/Models/Acronym.swift
@@ -1,6 +1,6 @@
 
 import Vapor
-import FluentSQLite
+import FluentPostgreSQL
 
 final class Acronym: Codable {
     var id: Int?
@@ -20,6 +20,6 @@ final class Acronym: Codable {
 //    public static var idKey: IDKey = \Acronym.id
 //}
 
-extension Acronym: SQLiteModel {}
+extension Acronym: PostgreSQLModel {}
 extension Acronym: Migration {}
 extension Acronym: Content {}
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,4 +1,4 @@
-import FluentSQLite
+import Flu
 import Vapor
 
 /// Called before your application initializes.
@@ -18,13 +18,29 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     services.register(middlewares)
 
     // Configure a SQLite database
-    let sqlite = try SQLiteDatabase(storage: .memory)
+    let sqlite = try SQLiteDatabase(storage: .file(path: "db.sqlite"))
 
     /// Register the configured SQLite database to the database config.
     var databases = DatabasesConfig()
-    databases.add(database: sqlite, as: .sqlite)
-        services.register(databases)
-
+    
+    let hostname = Environment.get("DATABASE_HOSTNAME")
+        ?? "localhost"
+    let username = Environment.get("DATABASE_USER") ?? "vapor"
+    let databaseName = Environment.get("DATABASE_DB") ?? "vapor"
+    let password = Environment.get("DATABASE_PASSWORD")
+        ?? "password"
+    // 3
+    let databaseConfig = PostgreSQLDatabaseConfig(
+        hostname: hostname,
+        username: username,
+        database: databaseName,
+        password: password)
+    // 4
+    let database = PostgreSQLDatabase(config: databaseConfig)
+    // 5
+    databases.add(database: database, as: .psql)
+    // 6
+    services.register(databases)
     /// Configure migrations
     var migrations = MigrationConfig()
     migrations.add(model: Acronym.self, database: .sqlite)
